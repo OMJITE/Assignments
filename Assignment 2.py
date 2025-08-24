72ffc7888f27884b0b275295bfcdaa29efd900f0
Om Jite <omjite92@gmail.com>
2025-08-24 11:53:24 +0530

    https://github.com/OMJITE/Assignments.git

---
+++ Assignment 2.py
@@ -0,0 +1,10 @@
+#TASK 1
+Number=int(input("Enter your number :"))
+if Number % 2 == 0:
+    print(Number,"is an even number.")
+else:
+    print(Number,"is an odd number.")
+#TASK 2
+a=sum(range(1,51))
+print("The sum of numbers from 1 to 50 is :",a, )
+print("Thank you")
